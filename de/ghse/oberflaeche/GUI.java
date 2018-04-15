76c2d3624b344780637d85e8b21e0d01c830eeef
andreas-buehrer <andreas-buehrer@andreasbuehrer-desktop>
2018-04-15 14:48:42 +0200

    Bug fix with saving files because action Listener structure parse Int

--- de/ghse/oberflaeche/GUI.java
+++ de/ghse/oberflaeche/GUI.java
@@ -1,6 +1,5 @@
 package de.ghse.oberflaeche;
 
-
 import de.ghse.steuerung.Steuerung;
 import java.awt.EventQueue;
 import java.awt.Graphics;
@@ -31,13 +30,12 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import javax.swing.JLabel;
 
-
-public class GUI extends JFrame implements MenuListener, ActionListener, ItemListener{
+public class GUI extends JFrame implements MenuListener, ActionListener, ItemListener {
 
 	String info2 = "420 x 512 Zahlen	20 Sekunden Laufzeit bei 24fps	Beschreibung: Dieses Programm stellt einen größer werdenden Kreis dar";
 	String data2 = "11101010110";
-	final int LEDS = 64; //Anzahl der LEDS
-	int layer = 0,countm,countn,button;
+	final int LEDS = 64; // Anzahl der LEDS
+	int layer = 0, countm, countn, button;
 	private JButton[] buttons;
 	private JTextField display;
 	private JLabel CurrentEbenetext;
@@ -46,20 +44,17 @@ public class GUI extends JFrame implements MenuListener, ActionListener, ItemLis
 	private JButton ebenedown;
 	private boolean[] geklickt = new boolean[LEDS];
 	private boolean[] matrix = new boolean[513];
-	private int CurrentEbene=0;
+	private int CurrentEbene = 0;
 	Steuerung obj = new Steuerung();
 	Boolean an_aus;
-	 
-	
-	
+
 	private JFrame frame;
 	private File file;
 
 	/**
 	 * Launch the application.
 	 */
-	
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -78,158 +73,150 @@ public class GUI extends JFrame implements MenuListener, ActionListener, ItemLis
 	 */
 	public GUI() {
 		initialize();
-		
-		
-	}//lol
+
+	}// lol
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		int frameWidth = 1000; 
+
+		int frameWidth = 1000;
 		int frameHeight = 1000;
-		
+
 		frame = new JFrame();
-		frame.setSize(frameWidth,frameHeight);
+		frame.setSize(frameWidth, frameHeight);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		
+
 		JLabel label = new JLabel("");
 		frame.getContentPane().add(label, BorderLayout.CENTER);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu File = new JMenu("File");
 		menuBar.add(File);
-		
+
 		JMenuItem OpenFile = new JMenuItem("Open File...");
 		File.add(OpenFile);
 		OpenFile.addActionListener(this);
-		
+
 		JMenuItem SaveAs = new JMenuItem("Save as...");
 		File.add(SaveAs);
 		SaveAs.addActionListener(this);
-		
+
 		JMenuItem Exit = new JMenuItem("Exit");
 		File.add(Exit);
 		Exit.addActionListener(this);
-		
-		
-	    //Button Panel
+
+		// Button Panel
 		for (int matrixsetup = 0; matrixsetup < 513; matrixsetup++) {// Setting up the the save array
-			matrix[matrixsetup]=false;
-			
+			matrix[matrixsetup] = false;
+
 		}
-	    	    
-	    
-	    buttons = new JButton[LEDS];
-	    
-	    JPanel buttonPanel = new JPanel();
-	    buttonPanel.setPreferredSize(new Dimension(200,900));
-	    buttonPanel.setLayout( new GridLayout(9,8,4,4));
-	    
-	    for (int i = 0; i < buttons.length; i++)
-	    {
-	        String LED = String.valueOf(i+1);  //+1 Damit die Buttons von 1-64 gezählt werden
-	        JButton button = new JButton(LED);
-	        
-	        button.setBackground(new Color(255,255,255));
-	        button.addActionListener( this );
-	        button.setMnemonic( LED.charAt(0) );
-	        
-	        buttons[i] = button;
-	        buttonPanel.add( buttons[i] );
-	    }
-	    
-	    output = new JButton("Weiter");
-	    buttonPanel.add(output);
-	    output.addActionListener(this);
-	    output.setBackground(new Color(255,70,0));
-	    
-	    
-	    ebeneup = new JButton("UP");
-	    buttonPanel.add(ebeneup);
-	    ebeneup.addActionListener(this);
-	    ebeneup.setBackground(new Color(255,70,0));
-	   
-	    	    
-	    ebenedown = new JButton("DOWN");
-	    buttonPanel.add(ebenedown);
-	    ebenedown.addActionListener(this);
-	    ebenedown.setBackground(new Color(255,70,0));
-	    
-	    
-	    int displayEbene=CurrentEbene+1;
-	    
-	    CurrentEbenetext= new JLabel("Ebene = "+displayEbene);
-	    
-	    buttonPanel.add(CurrentEbenetext);
 
-	    UIManager.put("Button.margin", new Insets(10, 10, 10, 10) ); //gibt die Form der Buttons vor
-	    
-	    frame.getContentPane().add(buttonPanel,BorderLayout.SOUTH);
-	    //Button Panel Setup zu Ende
-	    PaintPanel aniPanel = new PaintPanel();    // benötigt zum malen mit paintComponent graphics g
-	    frame.add(aniPanel);
-		
+		buttons = new JButton[LEDS];
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setPreferredSize(new Dimension(200, 900));
+		buttonPanel.setLayout(new GridLayout(9, 8, 4, 4));
+
+		for (int i = 0; i < buttons.length; i++) {
+			String LED = String.valueOf(i + 1); // +1 Damit die Buttons von 1-64 gezählt werden
+			JButton button = new JButton(LED);
+
+			button.setBackground(new Color(255, 255, 255));
+			button.addActionListener(this);
+			button.setMnemonic(LED.charAt(0));
+
+			buttons[i] = button;
+			buttonPanel.add(buttons[i]);
+		}
+
+		output = new JButton("Weiter");
+		buttonPanel.add(output);
+		output.addActionListener(this);
+		output.setBackground(new Color(255, 70, 0));
+
+		ebeneup = new JButton("Ebene hoch");
+		buttonPanel.add(ebeneup);
+		ebeneup.addActionListener(this);
+		ebeneup.setBackground(new Color(255, 70, 0));
+
+		ebenedown = new JButton("Ebene runter");
+		buttonPanel.add(ebenedown);
+		ebenedown.addActionListener(this);
+		ebenedown.setBackground(new Color(255, 70, 0));
+
+		int displayEbene = CurrentEbene + 1;
+
+		CurrentEbenetext = new JLabel("Ebene = " + displayEbene);
+
+		buttonPanel.add(CurrentEbenetext);
+
+		UIManager.put("Button.margin", new Insets(10, 10, 10, 10)); // gibt die Form der Buttons vor
+
+		frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		// Button Panel Setup zu Ende
+		PaintPanel aniPanel = new PaintPanel(); // benötigt zum malen mit paintComponent graphics g
+		frame.add(aniPanel);
+
 	}
-	
-	public class PaintPanel extends JPanel{
 
-        public void paintComponent(Graphics g){
+	public class PaintPanel extends JPanel {
 
-            g.setColor(Color.red);
-            //g.fillOval(150, 150, 200, 200);
+		public void paintComponent(Graphics g) {
 
-        }
-      }
-	
+			g.setColor(Color.red);
+			// g.fillOval(150, 150, 200, 200);
+
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		JFileChooser chooser = new JFileChooser("C:\\Users\\Miles\\Documents");
+
+		JFileChooser chooser = new JFileChooser();
 		String quelle = e.getActionCommand();
-		String data,info,line;
-		
-		if(quelle == "Exit") {
+		String data, info, line;
+
+		if (quelle == "Exit") {
 			System.exit(0);
 		}
-		
-		if(quelle == "Open File...") {
-			
-			int rueckgabewert = chooser.showDialog(null,"Open");
-			if(rueckgabewert == JFileChooser.APPROVE_OPTION) {
+
+		if (quelle == "Open File...") {
+
+			int rueckgabewert = chooser.showDialog(null, "Open");
+			if (rueckgabewert == JFileChooser.APPROVE_OPTION) {
 				file = chooser.getSelectedFile();
-				
+
 				try {
-				BufferedReader reader = new BufferedReader(new FileReader(file));
-				FileReader fileReader = new FileReader(file);
-				StringBuffer stringBuffer = new StringBuffer();
-				
-				info = reader.readLine();	//erste zeile lesen weil dort info steht
-			
-				while ((line = reader.readLine()) != null) {	//er macht mit zeile 2 weiter, liest solange bis eine leere zeile kommt
-					stringBuffer.append(line);
-					stringBuffer.append("\n");
-				}
-				fileReader.close();
-				data = stringBuffer.toString();
-				System.out.println("Info: " + info);
-				System.out.println("Daten: " + data);
-				
-					
+					BufferedReader reader = new BufferedReader(new FileReader(file));
+					FileReader fileReader = new FileReader(file);
+					StringBuffer stringBuffer = new StringBuffer();
+
+					info = reader.readLine(); // erste zeile lesen weil dort info steht
+
+					while ((line = reader.readLine()) != null) { // er macht mit zeile 2 weiter, liest solange bis eine
+																	// leere zeile kommt
+						stringBuffer.append(line);
+						stringBuffer.append("\n");
+					}
+					fileReader.close();
+					data = stringBuffer.toString();
+					System.out.println("Info: " + info);
+					System.out.println("Daten: " + data);
+
 				} catch (Exception error1) {
-					error1.printStackTrace();//nichts ausgeben
-				}					
-			}//end of if
-			
-		}//end of if
-		
-		if(quelle == "Save as...") {
-			
+					error1.printStackTrace();// nichts ausgeben
+				}
+			} // end of if
+
+		} // end of if
+
+		if (quelle == "Save as...") {
+
 			chooser.setSelectedFile(new File("*.cube"));
 			chooser.showSaveDialog(this);
 			File file = chooser.getSelectedFile();
@@ -238,115 +225,105 @@ public class GUI extends JFrame implements MenuListener, ActionListener, ItemLis
 				fw.write(info2);
 				fw.write(data2);
 				fw.close();
-				
-			} catch ( IOException error2) {
+
+			} catch (IOException error2) {
 				// TODO: handle exception
 			}
-			
+
 		}
-		ButtonPanelActionListener(quelle);	//übergibt string "quelle" an methode ButtonPanelActionListener
+		ButtonPanelActionListener(quelle); // übergibt string "quelle" an methode ButtonPanelActionListener
 	}
-	
-	
-	
+
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void menuCanceled(MenuEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void menuDeselected(MenuEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void menuSelected(MenuEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public void ButtonPanelActionListener(String quelle)  //actionlistener um herasuzufinden welcher button gedr�ckt wurde. jeder Button Teilt sich einen ActionListener
-	{   
-		//System.out.println(quelle);
-			int zahl=0;  
-		
-		//JButton source = (JButton)e.getSource(); //findet raus welcher Button den EventListener ausgel�st hat
-	      if (quelle== "Weiter") {
-	    	
-	    	
-	    	  
-	    	  
-	      }else if (quelle=="UP") {//UP Knopf
-	    	  if(CurrentEbene<7)
-	    		  {
-	    		  CurrentEbene++;
-	    		  EbeneUpdate();
-	    		  }
-	      }else if (quelle=="DOWN") {//DOWN Knopf
-	    	  if(CurrentEbene>0)
-	    	  {
-	    	  CurrentEbene--;
-	    	  EbeneUpdate();
-	    	  }
-	      }else {
-			
-		
-	    	 
-	    	  zahl = Integer.parseInt((quelle))-1;
-	    	  buttons[zahl].setBackground(new Color(2,2,52));
-	    	  if (!geklickt[zahl]) {
-	          geklickt[zahl] = true;  
-	          //display.replaceSelection(" | " + quelle);
-	          an_aus=true;
-	          //obj.knopfGedrueckt(button, an_aus); 			//Gibt Knopfdaten weiter
-	      }else {
-	    	  geklickt[zahl] = false;
-	    	  buttons[zahl].setBackground(new Color(255,255,255));
-	      }	
-	    	 
-	    	  
-	    }
-	      ButtonState(geklickt[zahl],zahl);  
+
 	}
 
-	void ButtonState(boolean state,int button_nr) { // Updated Button State Array
-		int CurrentOffset=(CurrentEbene)*64;
-		
-		matrix[button_nr+CurrentOffset]=state;
-		System.out.println("LED " +button_nr + " = 1");
-		
+	public void ButtonPanelActionListener(String quelle) // actionlistener um herasuzufinden welcher button gedr�ckt
+															// wurde. jeder Button Teilt sich einen ActionListener
+	{
+		// System.out.println(quelle);
+		int zahl = 0;
+
+		// JButton source = (JButton)e.getSource(); //findet raus welcher Button den
+		// EventListener ausgel�st hat
+		if (quelle == "Weiter") {
+
+		} else if (quelle == "Ebene hoch") {// UP Knopf
+			if (CurrentEbene < 7) {
+				CurrentEbene++;
+				EbeneUpdate();
+			}
+		} else if (quelle == "Ebene runter") {// DOWN Knopf
+			if (CurrentEbene > 0) {
+				CurrentEbene--;
+				EbeneUpdate();
+			}
+		} else if (quelle.length()<3) {
+			
+			
+			zahl=Integer.parseInt(quelle)-1;
+			buttons[zahl].setBackground(new Color(2, 2, 52));
+
+			if (!geklickt[zahl]) {
+
+				geklickt[zahl] = true;
+				// display.replaceSelection(" | " + quelle);
+				an_aus = true;
+				// obj.knopfGedrueckt(button, an_aus); //Gibt Knopfdaten weiter
+			} else {
+				geklickt[zahl] = false;
+				buttons[zahl].setBackground(new Color(255, 255, 255));
+			}
+
 		}
-	
-	void EbeneUpdate() { //Methode die die Buttons updated
-		
-		CurrentEbenetext.setText("Ebene = "+Integer.toString(CurrentEbene+1));//Display der derzeitigen Ebene
-		int CurrentOffset=(CurrentEbene)*64;//Button Offset
-		
-		for (int i = 0; i <64; i++) {
-			
-			if(matrix[i+CurrentOffset])	// Findet den Status der Knöpfe heraus
+		ButtonState(geklickt[zahl], zahl);
+	}
+
+	void ButtonState(boolean state, int button_nr) { // Updated Button State Array
+		int CurrentOffset = (CurrentEbene) * 64;
+
+		matrix[button_nr + CurrentOffset] = state;
+		System.out.println("LED " + button_nr + " = 1");
+
+	}
+
+	void EbeneUpdate() { // Methode die die Buttons updated
+
+		CurrentEbenetext.setText("Ebene = " + Integer.toString(CurrentEbene + 1));// Display der derzeitigen Ebene
+		int CurrentOffset = (CurrentEbene) * 64;// Button Offset
+
+		for (int i = 0; i < 64; i++) {
+
+			if (matrix[i + CurrentOffset]) // Findet den Status der Knöpfe heraus
 			{
-				geklickt[i]=true;
-			buttons[i].setBackground(new Color(2,2,52)); // Setzt den Button entsprechend
-			}else
-			{
-				geklickt[i]=false;
-				
-			buttons[i].setBackground(new Color(255,255,255));// Setzt den Button entsprechend
-			}						
-		}			
-	}	
-	
+				geklickt[i] = true;
+				buttons[i].setBackground(new Color(2, 2, 52)); // Setzt den Button entsprechend
+			} else {
+				geklickt[i] = false;
+
+				buttons[i].setBackground(new Color(255, 255, 255));// Setzt den Button entsprechend
+			}
+		}
+	}
+
 }
-
-	
-
-
